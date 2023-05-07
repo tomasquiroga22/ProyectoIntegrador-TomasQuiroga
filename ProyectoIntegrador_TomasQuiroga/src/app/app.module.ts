fc66b5fee5a42fc6ddedbda2e432c9ddff3db9b7
tomasquiroga22 <tomasquirogacode@gmail.com>
2023-05-07 20:36:47 -0300

    Prueba commit

--- ProyectoIntegrador_TomasQuiroga/src/app/app.module.ts
+++ ProyectoIntegrador_TomasQuiroga/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BannerComponent } from './components/banner/banner.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
+    AcercaDeComponent,
   ],
   imports: [
     BrowserModule,
