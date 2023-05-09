ae53308aefa1fc9b73fa681029881db4e716a812
tomasquiroga22 <tomasquirogacode@gmail.com>
2023-05-09 00:28:52 -0300

    experiencia y educacion

--- ProyectoIntegrador_TomasQuiroga/src/app/app.module.ts
+++ ProyectoIntegrador_TomasQuiroga/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     SocialComponent,
     BannerComponent,
     AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
   ],
   imports: [
     BrowserModule,
